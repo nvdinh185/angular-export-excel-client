d97fd26248e09ee6930d1c08c639e5ce57e4e0d3
nvdinh185 <nvdinh0766777123@gmail.com>
2020-05-18 09:36:33 +0700

    edit after export excel error

--- routes/resorce-route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const fileHandler = require('../handlers/file-handler');
-
-router.get('/get-templates/*'
-    , fileHandler.getTemplates       // dựa vào giá trị req.user.username trả thông tin user
-)
-
-module.exports = router;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'down-up-excel', pathMatch: 'full' },
+  { path: '', redirectTo: 'export-excel', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'departments-kpi',
@@ -11,10 +11,6 @@ const routes: Routes = [
   {
     path: 'export-excel',
     loadChildren: () => import('./export-excel/export-excel.module').then( m => m.ExportExcelPageModule)
-  },
-  {
-    path: 'down-up-excel',
-    loadChildren: () => import('./down-up-excel/down-up-excel.module').then( m => m.DownUpExcelPageModule)
   },
 
 ];
